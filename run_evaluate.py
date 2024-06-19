521160175718f9cb2a02bafe705fcb30e1d83395
KhaLee2307 <20520208@gm.uit.edu.vn>
2024-06-19 17:32:14 +0700

    Change the direct path to indirect

--- run_evaluate.py
+++ run_evaluate.py
@@ -16,54 +16,23 @@ if __name__ == "__main__":
 
     args = {
             'mode': 'predict_csv',
-            'pretrained_model': 'weights/nisqa.tar',
-            'data_dir': r'C:\Users\Name\Downloads\NISQA_Corpus',
-            'output_dir': r'C:\Users\Name\Downloads\NISQA_Corpus',
+            'pretrained_model': 'weights/nisqa_mos_only.tar',
+            'data_dir': r'./data/NISQA_Corpus',
+            'output_dir': r'./data/NISQA_Corpus',
             'csv_file': 'NISQA_corpus_file.csv',
             'csv_con': 'NISQA_corpus_con.csv',
             'csv_deg': 'filepath_deg',
             'csv_mos_val': 'mos',
-            'tr_num_workers': 6,
-            'tr_bs_val': 40,
+            'tr_num_workers': 2,
+            'tr_bs_val': 128,
             'ms_channel': None,
             }
             
     nisqa = nisqaModel(args)
+#     print(nisqa.model)
     nisqa.predict()
     nisqa.evaluate(
         mapping='first_order', 
-        do_print=True,
-        do_plot=True
+        do_print=False,
+        do_plot=False
         )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
